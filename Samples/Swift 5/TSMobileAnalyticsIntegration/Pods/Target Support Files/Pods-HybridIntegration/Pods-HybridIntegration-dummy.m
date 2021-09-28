c14b52341c49e544329e2b0e9dbd8ef2465d4e1d
Jack Nutting <jacknutting@mac.com>
2021-09-28 14:01:53 +0200

    Make update (5.2.2)

--- Samples/Swift 5/TSMobileAnalyticsIntegration/Pods/Target Support Files/Pods-HybridIntegration/Pods-HybridIntegration-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HybridIntegration : NSObject
-@end
-@implementation PodsDummy_Pods_HybridIntegration
-@end
--- Samples/Swift 5/TSMobileAnalyticsIntegration/Pods/Target Support Files/Pods-NativeIntegration/Pods-NativeIntegration-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NativeIntegration : NSObject
-@end
-@implementation PodsDummy_Pods_NativeIntegration
-@end
